5894c7fb34bee6467bfaac59f9250906f5ef6574
= <=>
2011-10-03 03:47:21 +1100

    Test output fixed; debug text procedure added

--- CC/main.cpp
+++ CC/main.cpp
@@ -4,10 +4,26 @@
 #include "test.h"
 #include "tokenizer.h"
 
+void Tokenize()
+{
+    Tokenizer t;
+    t.bind("first.c");
+
+    printf("Text\t(line, col)\tType\tValue\n");
+
+    while(t.get().type != TOK_EOF)
+    {
+        t.next().outputAsString(cout);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if(argc == 1)
+    {
         printf("%s\n", "No args found. You should enter some filename or --test to test the application");
+        Tokenize();
+    }
     else if(argc > 1)
     {
         if(strcmp(argv[1], "--test"))
@@ -19,9 +35,9 @@ int main(int argc, char *argv[])
 
             printf("Text\t(line, col)\tType\tValue\n");
 
-            while(true)
+            while(t.get().type != TOK_EOF)
             {
-                printf("%s", t.get().asString());
+                t.next().outputAsString(cout);
             }
         }
     }
