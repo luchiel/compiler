1b1eab32de08988c8063795d559adc6379320516
luchiel <luchiella@yandex.ru>
2011-10-19 01:13:49 +1100

    Tokens -> operations

--- CC/source/operations.cpp
+++ CC/source/operations.cpp
@@ -44,4 +44,42 @@ OperationGroups::~OperationGroups()
     //_unaryOps.clear();
 }
 
+string operationName(TokenType type)
+{
+    switch(type)
+    {
+        case TOK_L_SQUARE: return "[]";
+        case TOK_L_BRACKET: return "()";
+        case TOK_ARROW: return "->";
+        case TOK_DOT: return ".";
+        case TOK_INC: return "++";
+        case TOK_DEC: return "--";
+        case TOK_SIZEOF: return "[]";
+        case TOK_PLUS: return "+";
+        case TOK_MINUS: return "-";
+        case TOK_TILDA: return "~";
+        case TOK_AMP: return "&";
+        case TOK_ASTERISK: return "*";
+        case TOK_NOT: return "!";
+
+        case TOK_DIV: return "/";
+        case TOK_MOD: return "%";
+        case TOK_SHL: return "<<";
+        case TOK_SHR: return ">>";
+        case TOK_L: return "<";
+        case TOK_G: return ">";
+        case TOK_LE: return "<=";
+        case TOK_GE: return ">=";
+        case TOK_EQUAL: return "==";
+        case TOK_NOT_EQUAL: return "!=";
+        case TOK_XOR: return "^";
+        case TOK_OR: return "|";
+        case TOK_LOGICAl_AND: return "&&";
+        case TOK_LOGICAL_OR: return "||";
+
+        default:
+            throw OperationExpected();
+    }
+}
+
 }
